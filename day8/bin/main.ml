274a70a2cca6b22e522fc166da92d3275822f1e0
Mark Elvers <mark.elvers@tunbury.org>
2023-12-08 08:14:04 +0000

    Day 8 - part 2

--- day8/bin/main.ml
+++ day8/bin/main.ml
@@ -10,6 +10,15 @@ let read_input =
   in
   loop ic []
 
+let rec gcd u v =
+  if v <> 0 then (gcd v (u mod v))
+  else (abs u)
+
+let lcm m n =
+  match m, n with
+  | 0, _ | _, 0 -> 0
+  | m, n -> abs (m * n) / (gcd m n)
+
 let explode s = List.init (String.length s) (String.get s)
 
 module Maps = Map.Make (struct
@@ -27,16 +36,30 @@ let route, maps = List.fold_left (fun (route, maps) line ->
     | Text r :: _ -> (explode r), maps
     | _ -> route, maps) ([], Maps.empty) read_input
 
-let rec follow counter current full_route maps route =
-  if current = "ZZZ" then
+let ends = ["ZZZ"]
+
+let rec follow counter current ends rt =
+  if List.mem current ends then
     counter
-  else match route with
-  | [] -> follow counter current full_route maps full_route
+  else match rt with
+  | [] -> follow counter current ends route
   | hd :: tl ->
     let l, r = Maps.find current maps in
-    follow (counter + 1) (if hd = 'L' then l else r) full_route maps tl
+    follow (counter + 1) (if hd = 'L' then l else r) ends tl
 
-let count = follow 0 "AAA" route maps route
+let count = follow 0 "AAA" ends route
 
 let () = Printf.printf "steps = %i\n" count
+
+let starts, ends = Maps.fold (fun k _ (s, e) ->
+  match String.get k 2 with
+    | 'A' -> (k :: s, e)
+    | 'Z' -> (s, k :: e)
+    | _ -> (s, e)) maps ([], [])
+
+let lengths = List.map (fun start -> follow 0 start ends route) starts
+
+let part2 = List.fold_left (lcm) 1 lengths
+
+let () = Printf.printf "part2 = %i\n" part2
 
