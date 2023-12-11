05f45fd9fce29f9acd49e982e659fb6e0a12d80b
Mark Elvers <mark.elvers@tunbury.org>
2023-12-11 08:51:38 +0000

    Tidy up

--- day11/bin/main.ml
+++ day11/bin/main.ml
@@ -43,40 +43,21 @@ let expansion_x, expansion_y =
           if g.y = y then true else a) false galaxies)) y in
   x, y
 
-let () = List.iter (Printf.printf "%i\n") expansion_x
-let () = List.iter (Printf.printf "%i\n") expansion_y
-
-let apply_expansion_x x g =
-  List.fold_left (fun lst x ->
-      List.map (fun p -> if p.x > x
-                 then { x = p.x + 1; y = p.y }
+let apply_expansion g comp dist =
+  List.fold_left (fun lst v ->
+      List.map (fun p -> if comp p v
+                 then { x = p.x + dist.x; y = p.y + dist.y }
                  else p) lst
-  ) g (List.rev x)
+  ) g
 
-let apply_expansion_y y g =
-  List.fold_left (fun lst y ->
-      List.map (fun p -> if p.y > y
-                 then { x = p.x; y = p.y + 1 }
-                 else p) lst
-  ) g (List.rev y)
-
-let () = Printf.printf "before\n"
-let () = List.iter (fun p -> Printf.printf "%i,%i\n" p.x p.y) galaxies
-
-let galaxies = apply_expansion_x expansion_x galaxies
-let galaxies = apply_expansion_y expansion_y galaxies
-
-let () = Printf.printf "after\n"
-let () = List.iter (fun p -> Printf.printf "%i,%i\n" p.x p.y) galaxies
+let galaxies = apply_expansion galaxies (fun p1 p2 -> p1.x > p2) { x = 1; y = 0 } (List.rev expansion_x)
+let galaxies = apply_expansion galaxies (fun p1 p2 -> p1.y > p2) { x = 0; y = 1 } (List.rev expansion_y)
 
 let rec loop sum = function
   | [] -> sum
   | hd :: tl ->
-    let () = Printf.printf "head %i,%i" hd.x hd.y in
     let sum = List.fold_left (fun sum g ->
-        let dist = abs (g.y - hd.y) + abs (g.x - hd.x) in
-        let () = Printf.printf "to %i,%i is %i\n" g.x g.y dist in
-        sum + dist) sum tl in
+        sum + abs (g.y - hd.y) + abs (g.x - hd.x)) sum tl in
     loop sum tl
 
 let sum = loop 0 galaxies
@@ -89,58 +70,10 @@ let galaxies, _ = Universe.filter (fun _ ch -> ch = '#') universe
              |> Universe.to_list
              |> List.split
 
-let expansion_x, expansion_y =
-  let max, _ = Universe.max_binding universe in
-  let min, _ = Universe.min_binding universe in
-  let x = List.init (max.x - min.x) (fun x -> x + min.x) in
-  let y = List.init (max.y - min.y) (fun y -> y + min.y) in
-  let x = List.filter (fun x ->
-      not (List.fold_left (fun a g ->
-          if g.x = x then true else a) false galaxies)) x in
-  let y = List.filter (fun y ->
-      not (List.fold_left (fun a g ->
-          if g.y = y then true else a) false galaxies)) y in
-  x, y
-
-let () = List.iter (Printf.printf "%i\n") expansion_x
-let () = List.iter (Printf.printf "%i\n") expansion_y
-
-let apply_expansion_x x g =
-  List.fold_left (fun lst x ->
-      List.map (fun p -> if p.x > x
-                 then { x = p.x + 999999; y = p.y }
-                 else p) lst
-  ) g (List.rev x)
-
-let apply_expansion_y y g =
-  List.fold_left (fun lst y ->
-      List.map (fun p -> if p.y > y
-                 then { x = p.x; y = p.y + 999999 }
-                 else p) lst
-  ) g (List.rev y)
-
-let () = Printf.printf "before\n"
-let () = List.iter (fun p -> Printf.printf "%i,%i\n" p.x p.y) galaxies
-
-let galaxies = apply_expansion_x expansion_x galaxies
-let galaxies = apply_expansion_y expansion_y galaxies
-
-let () = Printf.printf "after\n"
-let () = List.iter (fun p -> Printf.printf "%i,%i\n" p.x p.y) galaxies
-
-let rec loop sum = function
-  | [] -> sum
-  | hd :: tl ->
-    let () = Printf.printf "head %i,%i" hd.x hd.y in
-    let sum = List.fold_left (fun sum g ->
-        let dist = abs (g.y - hd.y) + abs (g.x - hd.x) in
-        let () = Printf.printf "to %i,%i is %i\n" g.x g.y dist in
-        sum + dist) sum tl in
-    loop sum tl
+let galaxies = apply_expansion galaxies (fun p1 p2 -> p1.x > p2) { x = 999999; y = 0 } (List.rev expansion_x)
+let galaxies = apply_expansion galaxies (fun p1 p2 -> p1.y > p2) { x = 0; y = 999999 } (List.rev expansion_y)
 
 let sum = loop 0 galaxies
 
 let () = Printf.printf "part 2: %i\n" sum
-
-
 
