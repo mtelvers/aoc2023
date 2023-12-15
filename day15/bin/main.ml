ec4c1df51bc9ba54bc5dd500290c9a1d3db7a64c
Mark Elvers <mark.elvers@tunbury.org>
2023-12-15 08:55:35 +0000

    Day 15 - part 2

--- day15/bin/main.ml
+++ day15/bin/main.ml
@@ -21,3 +21,31 @@ let part1 =
   List.fold_left (fun sum s -> sum + (hash s)) 0 sequences
 
 let () = Printf.printf "part 1 %i\n" part1
+
+
+let hashmap = Array.init 256 (fun _ -> [])
+
+let () = List.iter (fun seq ->
+    let () = match String.index_from_opt seq 0 '=' with
+    | Some i ->
+      let lens = String.sub seq 0 i in
+      let n = int_of_string (String.sub seq (i + 1) (String.length seq - i - 1)) in
+      let h = hash lens in
+      if List.mem_assoc lens (Array.get hashmap h)
+      then Array.set hashmap h (List.map (fun (l, m) -> if l = lens then (l, n) else (l, m)) (Array.get hashmap h))
+      else Array.set hashmap h ((Array.get hashmap h) @ [(lens, n)])
+    | None -> () in
+    match String.index_from_opt seq 0 '-' with
+    | Some i ->
+      let lens = String.sub seq 0 i in
+      let h = hash lens in
+      Array.set hashmap h (List.remove_assoc lens (Array.get hashmap h))
+    | None -> ()) sequences
+
+let power = Array.mapi (fun i lst ->
+    List.mapi (fun j (_, n) ->
+        (i + 1) * n * (j + 1)) lst
+        |> List.fold_left ( + ) 0) hashmap
+    |> Array.fold_left ( + ) 0
+
+let () = Printf.printf "part 2 %i\n" power
