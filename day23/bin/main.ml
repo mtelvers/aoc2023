97e48f573575198ebb2ad4fd39dc6ac964bfc4f4
Mark Elvers <mark.elvers@tunbury.org>
2023-12-23 09:08:55 +0000

    Using bigarray

--- day23/bin/main.ml
+++ day23/bin/main.ml
@@ -1,3 +1,5 @@
+open Bigarray
+
 let read_input =
   let ic = open_in "input" in
   let rec loop input lines =
@@ -10,28 +12,30 @@ let read_input =
   in
   loop ic []
 
-let rec pow a = function
-  | 0 -> 1
-  | 1 -> a
-  | n ->
-    let b = pow a (n / 2) in
-    b * b * (if n mod 2 = 0 then 1 else a)
-
 type coord = {
   y : int;
   x : int;
 }
 
-module Island = Map.Make (struct
-  type t = coord
-  let compare = compare
-end)
+let read_input = List.rev read_input
+let length = String.length (List.hd read_input)
 
-let island, _ = List.fold_left (fun (island, location) line ->
-    String.fold_left (fun (island, location) ch ->
-      (Island.add location ch island), { x = location.x + 1; y = location.y }
-    ) (island, { x = 1; y = location.y + 1 }) line
-  ) (Island.empty, { x = 1; y = 0 }) (List.rev read_input)
+let island = Array2.init Char C_layout length length (fun x y -> String.get (List.nth read_input y) x)
+
+let print ?(route=[]) i =
+  let () = Printf.printf "print\n" in
+    for y = 0 to length - 1 do
+      for x = 0 to length - 1 do
+        Printf.printf "%c" (if List.mem { x; y } route then 'O' else i.{ x, y })
+      done ;
+      Printf.printf "\n" ;
+    done ;
+    Printf.printf "\n"
+
+let () = print island
+
+let start = { x = 1; y = 0 }
+let finish = { x = length - 2; y = length - 1 }
 
 let neighbours p =
   [
@@ -41,40 +45,64 @@ let neighbours p =
     { x = p.x    ; y = p.y - 1 };
   ]
 
-let print ?(route=[]) f =
-  let _ = Island.fold (fun k v y ->
-    let () = if k.y != y then Printf.printf "\n" in
-    let () = Printf.printf "%c" (if List.mem k route then 'O' else v) in
-    k.y) f 1 in
-  Printf.printf "\n\n"
+let count ch i =
+  let rec loopy c x = function
+    | 0 -> c
+    | y -> loopy (if i.{ x - 1, y - 1 } = ch then c + 1 else c) x (y - 1) in
+  let rec loopx c = function
+    | 0 -> c
+    | x -> loopx (loopy c x length) (x - 1) in
+  loopx 0 length
 
-let () = print island
-
-let tl, _ = Island.min_binding island
-let start = { x = tl.x + 1; y = tl.y }
-let br, _ = Island.max_binding island
-let finish = { x = br.x - 1; y = br.y }
-
-let rec loop route =
-  let pos = List.hd route in
+let rec loop i pos =
   if pos = finish
-  then ((List.length route) - 1)
+  then count 'O' i
   else 
     let options = neighbours pos in
     let options = List.filter (fun p ->
-      not (List.mem p route) &&
-      match Island.find_opt p island with
-      | None -> false
-      | Some '#' -> false
-      | Some '.' -> true
-      | Some '>' when p.x - 1 = pos.x -> true
-      | Some '<' when p.x + 1 = pos.x -> true
-      | Some '^' when p.y + 1 = pos.y -> true
-      | Some 'v' when p.y - 1 = pos.y -> true
+      p.x >= 0 && p.x < length &&
+      p.y >= 0 && p.y < length &&
+      match i.{ p.x, p.y } with
+      | '.' -> true
+      | '>' when p.x - 1 = pos.x -> true
+      | '<' when p.x + 1 = pos.x -> true
+      | '^' when p.y + 1 = pos.y -> true
+      | 'v' when p.y - 1 = pos.y -> true
       | _ -> false
     ) options in
-    List.fold_left (fun longest p -> max longest (loop (p :: route))) 0 options
+    List.fold_left (fun longest p ->
+        let copy = Array2.create Char C_layout length length in
+        let () = Array2.blit i copy in
+        let () = copy.{ p.x, p.y } <- 'O' in
+        max longest (loop copy p)) 0 options
 
-let part1 = loop [start]
+let part1 = loop island start
 
 let () = Printf.printf "part 1 = %i\n" part1
+let () = flush stdout
+
+
+
+let rec loop i pos =
+(*  let () = print i in *)
+  if pos = finish
+  then count 'O' i
+  else 
+    let options = neighbours pos in
+    let options = List.filter (fun p ->
+      p.x >= 0 && p.x < length &&
+      p.y >= 0 && p.y < length &&
+      match i.{ p.x, p.y } with
+      | '<' | '^' | 'v' | '>' | '.' -> true
+      | _ -> false
+    ) options in
+    List.fold_left (fun longest p ->
+        let copy = Array2.create Char C_layout length length in
+        let () = Array2.blit i copy in
+        let () = copy.{ p.x, p.y } <- 'O' in
+        max longest (loop copy p)) 0 options
+
+let part2 = loop island start
+
+let () = Printf.printf "part 2 = %i\n" part2
+let () = flush stdout
